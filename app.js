51c2628f38a8705493cfd50661f6cd42d22d84bb
Murph Grainger <murph.grainger@gmail.com>
2017-01-31 09:39:30 -0700

    changed start command

--- app.js
+++ app.js
@@ -36,15 +36,15 @@ app.use('*', function(req, res) {
     })
 })
 
-// error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    const response = {
+        message: err.message
+    }
+    if (req.app.get('env') === 'development') {
+        response.stack = err.stack
+    }
 
-    // render the error page
-    res.status(err.status || 500);
-    res.send(err);
+    res.status(err.status || 500).json(response);
 });
 
 module.exports = app;
