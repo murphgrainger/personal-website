30fb9474352a1c402bd6714b1958d074c31a5eea
Murph Grainger <murph.grainger@gmail.com>
2017-01-31 18:37:08 -0700

    added custom color theme

--- client/app/app.config.js
+++ client/app/app.config.js
@@ -7,9 +7,29 @@
 
         $locationProvider.html5Mode(true)
 
+        var myTheme = {
+            '50': '#e9f7f9',
+            '100': '#d5eff4',
+            '200': '#c0e8ef',
+            '300': '#ace0ea',
+            '400': '#97d9e5',
+            '500': '#83d1e0',
+            '600': '#6fc9db',
+            '700': '#5ac2d6',
+            '800': '#46bad1',
+            '900': '#33b2ca',
+            'A100': '#fdfeff',
+            'A200': '#ffffff',
+            'A400': '#ffffff',
+            'A700': '#2da0b6'
+        };
+        $mdThemingProvider
+            .definePalette('myTheme',
+                myTheme);
+
         $mdThemingProvider
             .theme('default')
-            .primaryPalette('green')
+            .primaryPalette('myTheme')
             .warnPalette('red')
             .backgroundPalette('grey');
 
